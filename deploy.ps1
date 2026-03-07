6aef7712ce3f3b2850cdff3b7e1ce9d4fead9bd4
Dani Santos <daniramossantos9@gmail.com>
2026-03-07 19:14:56 +0000

    fixed deploy

--- deploy.ps1
+++ deploy.ps1
@@ -1,44 +1,18 @@
 param(
-    [ValidateSet("Portal", "Website", "Cloud")]
+    [ValidateSet("portal", "www", "cloud")]
     [Parameter(Mandatory = $true)]
-    [string]$Target
+    [string]$AppName
 )
 
-# Map folder names to CapRover app names
-$AppNameMap = @{
-    "Portal"  = "portal"
-    "Website" = "www"
-    "Cloud"   = "cloud"
-}
-$AppName = $AppNameMap[$Target]
+# 1. Create new git repository
+git init -b main
 
-# Generate a unique branch name
-$BranchName = "deploy-$($Target.ToLower())-$(Get-Date -Format 'yyyyMMddHHmmss')"
-
-# 1. Create new branch
-git checkout -b $BranchName
-
-# 2. Delete everything except 'target' and '.git'
-Get-ChildItem -Force | Where-Object {
-    $_.Name -ne $Target -and $_.Name -ne '.git'
-} | Remove-Item -Recurse -Force
-
-# 3. Move target contents to root
-Move-Item -Path $Target\* -Destination .
-
-# 4. Remove empty folder
-Remove-Item $Target
-
-# 5. Commit changes
+# 2. Commit changes
 git add .
-git commit -m "Deploy $Target"
+git commit -m "Deploy $AppName"
 
-# 6. Deploy with CapRover
-caprover deploy -n xvirus -a $AppName -b $BranchName
+# 3. Deploy with CapRover
+caprover deploy -n xvirus -a $AppName -b main
 
-# 7. Return to previous branch
-$previous = git rev-parse --abbrev-ref @{-1}
-git checkout $previous
-
-# 8. Delete the temporary branch
-git branch -D $BranchName
+# 4. Delete the new git repository
+Remove-Item -Recurse -Force .git
