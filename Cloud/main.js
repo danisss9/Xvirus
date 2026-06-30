8b173e0058a501f41fb84e842e59f33334808def
Dani Santos <daniramossantos9@gmail.com>
2026-06-30 22:20:43 +0100

    update database

--- Cloud/main.js
+++ Cloud/main.js
@@ -344,7 +344,7 @@ app.get('/api/updateInfo', (req, res) => {
       description: 'Main malware database',
     },
     dailydb: {
-      version: 15208,
+      version: 15209,
       downloadUrl: 'https://cloud.xvirus.net/database/dailylist.db',
       description: 'Malware database updated daily',
     },
@@ -354,7 +354,7 @@ app.get('/api/updateInfo', (req, res) => {
       description: 'Main whitelist database',
     },
     dailywldb: {
-      version: 2938,
+      version: 2939,
       downloadUrl: 'https://cloud.xvirus.net/database/dailywl.db',
       description: 'Whitelist database updated daily',
     },
